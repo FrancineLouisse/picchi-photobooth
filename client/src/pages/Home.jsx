3da8835641d4d96ab031244f44c15ce5378366c8
Francine <francinemiranda421@gmail.com>
2026-07-02 22:09:57 +0800

    Home page UI

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,44 @@
+import "../styles/Home.css";
+import { useNavigate } from "react-router-dom";
+import Button from "../components/Button";
+import logo from "../assets/logo/logo.png";
+import camera from "../assets/images/camera.png";
+import smile from "../assets/images/smiley.png";
+import stars from "../assets/images/star.png";
+import polaroid from "../assets/images/polaroid.png";
+
 function Home() {
-  return <h1>Home Page</h1>;
+  const navigate = useNavigate();
+
+  return (
+    <main className="home">
+
+      {/* Decorations */}
+      <img src={camera} alt="" className="camera float" />
+      <img src={smile} alt="" className="smile float" />
+      <img src={stars} alt="" className="stars float" />
+      <img src={polaroid} alt="" className="polaroid float" />
+
+      <div className="home-content">
+
+        <img
+          src={logo}
+          alt="PicChi Logo"
+          className="logo"
+        />
+
+        <h2 className="tagline">
+          Snap the Cuteness!
+        </h2>
+
+        <Button onClick={() => navigate("/camera")}>
+          START
+        </Button>
+
+      </div>
+
+    </main>
+  );
 }
 
 export default Home;
